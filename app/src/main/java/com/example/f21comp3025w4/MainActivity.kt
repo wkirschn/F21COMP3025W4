d5e491b7013a692cfbe90323533b560c60e228a2
Wyatt <wkirschn@uoguelph.ca>
2021-11-10 15:23:01 -0500

    Initial

--- app/src/main/java/com/example/f21comp3025w4/MainActivity.kt
+++ app/src/main/java/com/example/f21comp3025w4/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.f21comp3025w4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.f21comp3025w4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.passDetailsButton.setOnClickListener {
+            var intent = Intent(this, PassDetailsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/f21comp3025w4/PassDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.f21comp3025w4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PassDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pass_details)
+    }
+}
