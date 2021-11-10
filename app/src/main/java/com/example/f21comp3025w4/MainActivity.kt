8aa0d715ab175e86bef2b2f83c7635a5ee74e35d
Wyatt <wkirschn@uoguelph.ca>
2021-11-10 13:29:37 -0500

    Initial

---
+++ app/src/main/java/com/example/f21comp3025w4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.f21comp3025w4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
